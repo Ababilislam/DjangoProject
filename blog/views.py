cb02cc5578b067fb9782f58528d176e6357ff6d3
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-03-27 12:18:04 +0600

    base template added to the projects

--- blog/views.py
+++ blog/views.py
@@ -4,22 +4,23 @@ from django.http import HttpResponse
 # Create your views here.
 posts = [
     {
-        'author':'ab',
-        'title':'blog post1',
-        'content':'first post of the content',
+        'author': 'ab',
+        'title': 'blog post1',
+        'content': 'first post of the content',
         'date_posted': 'august 28, 2023'
     },
-{
-        'author':'udoy',
-        'title':'blog post2',
-        'content':'second post of the content',
+    {
+        'author': 'udoy',
+        'title': 'blog post2',
+        'content': 'second post of the content',
         'date_posted': 'august 30, 2023'
     }
 ]
 
+
 def home(request):
     context = {
-        'posts':posts
+        'posts': posts
     }
     return render(request, 'blog/Home.html', context)
 
