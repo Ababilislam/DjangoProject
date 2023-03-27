2c5d869596a56af5469474f83e9841f4cae8b233
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-03-27 16:34:45 +0600

    model add to admin and model apply

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
-posts = [
-    {
-        'author': 'ab',
-        'title': 'blog post1',
-        'content': 'first post of the content',
-        'date_posted': 'august 28, 2023'
-    },
-    {
-        'author': 'udoy',
-        'title': 'blog post2',
-        'content': 'second post of the content',
-        'date_posted': 'august 30, 2023'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/Home.html', context)
 
