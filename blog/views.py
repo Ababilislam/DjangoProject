364aa7508a467d4e82f03ad571265745e2ba11dd
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-03-27 11:57:44 +0600

    in this commit home had a post

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author':'ab',
+        'title':'blog post1',
+        'content':'first post of the content',
+        'date_posted': 'august 28, 2023'
+    },
+{
+        'author':'udoy',
+        'title':'blog post2',
+        'content':'second post of the content',
+        'date_posted': 'august 30, 2023'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> this is home page</h1>')
-    # return render(request, 'blog/Home/html', name="home")
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/Home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h2>this is about page</h2>')
+    return render(request, 'blog/about.html')
